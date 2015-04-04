86f63934e2ebcf2e55f88bdc0f8ff779ce7320b4
CompatibL <team@compatibl.com>
2015-04-04 18:45:26 -0400

    Added more test placeholders.

---
+++ dotcpp/cpp/cl/system/CppObjectTests.hpp
@@ -0,0 +1,41 @@
+﻿/*
+Copyright (C) 2003-2015 CompatibL
+
+This file is part of DotCpp (.C++), an open source implementation of
+selected .NET class library APIs in native C++ with optional support
+for algorithmic differentiation (AD), available from
+
+    http://github.com/dotcpp/dotcpp (source)
+    http://dotcpp.org (documentation)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#ifndef __cl_system_CppObjectTests_hpp__
+#define __cl_system_CppObjectTests_hpp__
+
+#include <cl/system/CppObject.hpp>
+
+namespace cl
+{
+    /// <summary>String tests.</summary>
+    class CL_SYSTEM CppObjectTests
+    {
+    public: // METHODS
+
+        /// <summary>Test create methods.</summary>
+        void testCreate();
+    };
+}
+
+#endif  // __cl_system_CppObjectTests_hpp__
