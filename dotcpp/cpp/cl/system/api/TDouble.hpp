3b04f5d98f11048db378c2e21b2c03423b937f61
CompatibL <team@compatibl.com>
2019-02-25 22:20:56 -0500

    Removed arithmetic operations from tdouble, other minor changes.
    (cherry picked from commit b142f9df908c12e424c3bab4f355c22b2ee78173)

--- dotcpp/cpp/cl/system/api/TDouble.hpp
+++ dotcpp/cpp/cl/system/api/TDouble.hpp
@@ -28,287 +28,48 @@ limitations under the License.
 
 namespace cl
 {
-    using TDouble = double; using tdouble = double; //!!! Temporary, later define as TapeLib tdouble
-    // class TDouble; using tdouble = TDouble;
-    class TString; using tstring = TString;
-
-/*
-    /// <summary>Immutable double type with AD support designed
-    /// to serve as a drop-in replacement to native double.</summary>
-    class CL_SYSTEM TDouble
+    /// <summary>Nullable double type with arithmetics support.
+    /// In AD mode, this class contains a tape pointer.</summary>
+    class tdouble
     {
-    public:
+        double value_ = Empty;
 
-        /// <summary>AD implementation type, typedef according to this library's naming conventions.</summary>
-#ifdef CL_DOUBLE_CPPAD
-        typedef CppAD::AD<double> ImplType;
-#elif defined CL_DOUBLE_ADOLC
-        typedef adouble ImplType;
-#else
-        typedef double ImplType;
-#endif
+    public: //  CONSTANTS
 
-        /// <summary>AD implementation type, alternative typedef required by STL.</summary>
-        typedef ImplType value_type;
+        /// <summary>Empty value.</summary>
+        static const double Empty;
 
-    private:
+        /// <summary>Tolerance used in comparison of doubles.</summary>
+        static const double Tolerance;
 
-        /// <summary> The friend class to conversations </summary>
-        template <typename, typename, typename, typename, typename, typename, typename, typename >
-        friend struct cl::TDoubleConvert;
+    public: //  CONSTRUCTORS
 
-//!! Should we have this, can break AD when misused; or it could stop recording when invoked
-#ifdef CL_DOUBLE_CAN_GET_VALUE
-        template <typename TapeType>
-        friend inline typename TapeType::value_type const&  cl::detail::cvalue(TapeType const& tapetype);
+        /// <summary>Create with empty value.</summary>
+        tdouble() : value_(Empty) {}
 
-        template <typename TapeType>
-        friend inline typename TapeType::value_type& cl::detail::value(TapeType& tapetype);
-#endif
+        /// <summary>Explicit constructor from double.</summary>
+        tdouble(double rhs) : value_(rhs) {}
 
-    public: // TYPEDEFS
+    public: //  METHODS
 
-        /// <summary>Explicit conversion operator from an arbitrary type.</summary>
-        template <typename Type>
-        explicit TDouble(Type const& rhs)
-            : value_()
-        {
-            cl::TDoubleConvert<Type, value_type>::convert(*this, rhs);
-        }
-
-        /// <summary>Assignment from other type.</summary>
-        template <typename Type>
-        inline TDouble& operator=(Type const& rhs)
-        {
-            cl::TDoubleConvert<Type, value_type>::convert(*this, rhs);
-            return *this;
-        }
-
-//!! Should we have this, can break AD when misused; or it could stop recording when invoked
-#ifdef CL_DOUBLE_CAN_GET_VALUE
-        inline value_type value() const
-        {
-            return value_;
-        }
-#endif
-
-    private: // PRIVATE
-
-        /// <summary>Return cast value is it's arithmetic.</summary>
-        template <typename Type>
-        inline Type get__(std::true_type, std::false_type) const
-        {
-#if defined CL_COMPILE_TIME_DEBUG
-#   pragma message ("get__ std::true_type, std::false_type " __FUNCSIG__)
-#endif
-            return static_cast<Type>(ext::Value(this->value_));
-        }
-
-        template <typename Type>
-        inline Type get__(std::true_type, std::true_type) const
-        {
-#if defined CL_COMPILE_TIME_DEBUG
-#   pragma message ("get__ std::true_type, std::true_type " __FUNCSIG__)
-#endif
-            return static_cast<Type>(this->value_);
-        }
-
-        /// <summary>Return cast value. Try to create instance Typed.</summary>
-        template <typename Type>
-        inline Type get__(std::false_type, std::false_type) const
-        {
-#if defined CL_COMPILE_TIME_DEBUG
-#   pragma message ("get__ std::false_type, std::false_type " __FUNCSIG__)
-#endif
-            // Would cause a recursive call
-            static_assert(false, "Conversion to this type isn't implemented");
-            return Type();
-        }
-
-        template <typename Type>
-        inline Type get__(std::false_type, std::true_type) const
-        {
-#if defined CL_COMPILE_TIME_DEBUG
-#   pragma message ("get__ std::false_type, std::true_type " __FUNCSIG__)
-#endif
-            return Type(value_);
-        }
-    public:
-
-//!! Should we select one option and stay with it?
-#ifndef CL_EXPLICIT_NATIVE_CONVERSION
-
-        // This should skip message
-        template <typename Type>
-        inline void to(std::false_type) const {   }
-
-        // This should show message
-        template <typename Type>
-        inline void to(std::true_type) const
-        {
-#pragma message ("Possible lost adjoint functionality in case: " __FUNCSIG__)
-        }
-
-#if defined CL_DOUBLE_CPPAD
-        inline operator value_type() const
-        {
-            return value_;
-        }
-#endif
-
-        // Try to convert explicitly if this is arithmetic
-        template <typename Type>
-        inline explicit operator Type() const
-        {
-#if defined CL_COMPILE_TIME_DEBUG
-#   pragma message ("convert compilation for: " __FUNCSIG__)
-#endif
-            typedef typename std::remove_volatile<typename std::remove_const<Type>::type >::type native_type;
-
-            enum {
-                is_sm = std::is_same<native_type, value_type >::value
-                , is_constructible_from_value = std::is_constructible <native_type, value_type>::value
-                , is_arithmetic_or_contructible =
-                        std::is_arithmetic<Type>::value
-                            || std::is_constructible <native_type, typename cl::remove_ad<value_type>::type >::value
-            };
-
-            typedef std::integral_constant<bool, is_sm || is_constructible_from_value > is_value_type;
-            typedef std::integral_constant<bool, is_arithmetic_or_contructible > is_arithm_or_contrictible;
-
-            //  We should show message currenlty in cases when we can create
-            // class from double type but not from QlDouble
-            to<Type>(std::integral_constant<bool, is_arithmetic_or_contructible
-                && !std::is_arithmetic<Type>::value >());
-
-            return this->get__<Type>(is_arithm_or_contrictible(), is_value_type());
-        }
-#else
-        inline explicit operator int() const { return (int)value_; }
-        inline explicit operator value_type() const { return value_; }
-        inline explicit operator unsigned int() const { return (unsigned int)value_; }
-        inline explicit operator unsigned long() const { return (unsigned long)value_; }
-        inline explicit operator long() const { return (long)value_; }
-#endif
-
-    private: // FIELDS
-
-        value_type value_;
-
-    public: // CONSTRUCTORS
-
-        inline TDouble() : value_() {}
-
-        /// <summary>Implicit constructor from double.</summary>
-        inline TDouble(value_type rhs) : value_(rhs) {}
-
-//!!! Should this include other CL_DOUBLE_* options?
-#if defined CL_DOUBLE_CPPAD
-        inline TDouble(double rhs) : value_(rhs) {}
-#endif
-
-    public: // METHODS
+        /// <summary>Returns true if the field is empty.</summary>
+        bool isEmpty() const { return value_ == Empty; }
 
         /// <summary>Convert to string.</summary>
-        tstring ToString() const;
+        std::string toString() const;
 
-    public: // OPERATORS
+        /// <summary>Returns true if not empty.</summary>
+        bool IsSet() const { return value_ != Empty; }
 
-        /// <summary>Negation operator.</summary>
-        inline bool operator!() const { return value_ == 0.0; } //!! Provide special treatment for AD
+    public: //  OPERATORS
+
+        /// <summary>Conversion operator.</summary>
+        operator double() const { return value_; }
 
         /// <summary>Assignment of native double.</summary>
-        inline TDouble& operator=(double rhs) { value_ = rhs; return *this; }
-
-        /// <summary>Adds rhs to self.</summary>
-        inline TDouble& operator+=(const TDouble& rhs) { value_ += rhs.value_; return *this; }
-
-        /// <summary>Adds rhs to self.</summary>
-        inline TDouble& operator+=(double rhs) { value_ += rhs; return *this; }
-
-        /// <summary>Subtracts rhs from self.</summary>
-        inline TDouble& operator-=(const TDouble& rhs) { value_ -= rhs.value_; return *this; }
-
-        /// <summary>Subtracts rhs from self.</summary>
-        inline TDouble& operator-=(double rhs) { value_ -= rhs; return *this; }
-
-        /// <summary>Multiplies self by rhs.</summary>
-        inline TDouble& operator*=(const TDouble& rhs) { value_ *= rhs.value_; return *this; }
-
-        /// <summary>Multiplies self by rhs.</summary>
-        inline TDouble& operator*=(double rhs) { value_ *= rhs; return *this; }
-
-        /// <summary>Divides self by rhs.</summary>
-        inline TDouble& operator/=(const TDouble& rhs) { value_ /= rhs.value_; return *this; }
-
-        /// <summary>Divides self by rhs.</summary>
-        inline TDouble& operator/=(double rhs) { value_ /= rhs; return *this; }
-
-        /// <summary>Returns a copy if self.</summary>
-        inline TDouble operator+() const { return TDouble(value_); }
-
-        /// <summary>Returns the negative of self.</summary>
-        inline TDouble operator-() const { return TDouble(-value_); }
-
-        /// <summary>Returns true if self is equal to rhs.</summary>
-        inline bool operator==(const TDouble& rhs) const { return value_ == rhs.value_; }
-
-        /// <summary>Returns true if self is equal to rhs.</summary>
-        inline bool operator==(double rhs) const { return value_ == rhs; }
-
-        /// <summary>Returns true if self is not equal to rhs.</summary>
-        inline bool operator!=(const TDouble& rhs) const { return value_ != rhs.value_; }
-
-        /// <summary>Returns true if self is not equal to rhs.</summary>
-        inline bool operator!=(double rhs) const { return value_ != rhs; }
-
-        /// <summary>Returns true if self is less than rhs.</summary>
-        inline bool operator<(const TDouble& rhs) const { return value_ < rhs.value_; }
-
-        /// <summary>Returns true if self is less than rhs.</summary>
-        inline bool operator<(double rhs) const { return value_ < rhs; }
-
-        /// <summary>Returns true if self is less than or equal to rhs.</summary>
-        inline bool operator<=(const TDouble& rhs) const { return value_ <= rhs.value_; }
-
-        /// <summary>Returns true if self is less than or equal to rhs.</summary>
-        inline bool operator<=(double rhs) const { return value_ <= rhs; }
-
-        /// <summary>Returns true if self is more than rhs.</summary>
-        inline bool operator>(const TDouble& rhs) const { return value_ > rhs.value_; }
-
-        /// <summary>Returns true if self is more than rhs.</summary>
-        inline bool operator>(double rhs) const { return value_ > rhs; }
-
-        /// <summary>Returns true if self is more than or equal to rhs.</summary>
-        inline bool operator>=(const TDouble& rhs) const { return value_ >= rhs.value_; }
-
-        /// <summary>Returns true if self is more than or equal to rhs.</summary>
-        inline bool operator>=(double rhs) const { return value_ >= rhs; }
-
-        /// <summary>Prefix incrementation.</summary>
-        inline TDouble& operator++() {   value_ += 1.0; return *this; }
-
-        /// <summary>Postfix incrementation.</summary>
-        inline TDouble operator++(int) { TDouble result(value_); ++(*this);  return result; }
-
-        /// <summary>Prefix decrementation.</summary>
-        inline TDouble& operator--() { value_ -= 1.0; return *this; }
-
-        /// <summary>Postfix decrementation.</summary>
-        inline TDouble operator--(int) { TDouble result(value_); --(*this);  return result; }
-
-    private:
-
-        template <typename Type>
-        inline static ImplType
-        Value(Type const& obj, std::true_type) { return obj.value_; }
-
-        template <typename Type>
-        inline static ImplType
-        Value(Type const& obj, std::false_type) { return static_cast<ImplType>(obj); }
+        tdouble& operator=(double rhs) { value_ = rhs; return *this; }
     };
-*/
+
 }
 
 #endif // cl_system_TDouble_hpp
