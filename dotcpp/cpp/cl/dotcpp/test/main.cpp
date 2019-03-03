f25f5ee87d57de4c3f2135f40975ffa172bf6c6f
CompatibL <team@compatibl.com>
2019-03-03 02:05:15 -0500

    Converted main.cpp to unit test runner.

--- dotcpp/cpp/cl/dotcpp/test/main.cpp
+++ dotcpp/cpp/cl/dotcpp/test/main.cpp
@@ -8,16 +8,51 @@ license from CompatibL and with the inclusion of this copyright notice.
 
 #include <cl/dotcpp/test/implement.hpp>
 
-int main()
-{
-    try
-    {
-        std::cout << "Hello world" << std::endl;
-    }
-    catch(std::exception e)
-    {
-        std::cout << e.what() << std::endl;
-    }
+//#include <cl/dotcpp/test/system/collections/generic/DictionaryTest.hpp>
+//#include <cl/dotcpp/test/system/collections/generic/HashSetTest.hpp>
+#include <cl/dotcpp/test/system/collections/generic/ListTest.hpp>
+//#include <cl/dotcpp/test/system/collections/generic/QueueTest.hpp>
+//#include <cl/dotcpp/test/system/collections/generic/SortedDictionaryTest.hpp>
+//#include <cl/dotcpp/test/system/collections/generic/SortedListTest.hpp>
+//#include <cl/dotcpp/test/system/collections/generic/SortedSetTest.hpp>
+//#include <cl/dotcpp/test/system/collections/generic/StackTest.hpp>
 
-    return 0;
+#include <cl/dotcpp/test/system/DoubleTest.hpp>
+#include <cl/dotcpp/test/system/ObjectTest.hpp>
+#include <cl/dotcpp/test/system/PtrTest.hpp>
+#include <cl/dotcpp/test/system/StringTest.hpp>
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/impl/unit_test_main.ipp>
+
+using namespace boost::unit_test_framework;
+
+
+struct pause
+{
+	~pause()
+	{
+		system("PAUSE");
+	}
+};
+
+static pause pause_;
+
+
+test_suite* init_unit_test_suite(int, char*[])
+{
+	std::string header =
+		"Testing Cl.DotCpp";
+	std::string rule = std::string(35, '=');
+
+	BOOST_TEST_MESSAGE(rule);
+	BOOST_TEST_MESSAGE(header);
+	BOOST_TEST_MESSAGE(rule);
+	test_suite* test = BOOST_TEST_SUITE("Cl DotCpp test suite");
+
+	test->add(cl::ListTest::ListTestSuite());
+	test->add(cl::DoubleTest::DoubleTestSuite());
+
+	return test;
 }
+
