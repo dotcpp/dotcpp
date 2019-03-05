f5b089f0b172299d01a205fd88480b79dcc622ee
CompatibL <team@compatibl.com>
2019-03-05 16:22:33 -0500

    Applied new copyright header that does not mention algorithmic differentiation.

--- dotcpp/cpp/cl/dotcpp/main/system/Double.hpp
+++ dotcpp/cpp/cl/dotcpp/main/system/Double.hpp
@@ -1,9 +1,9 @@
 /*
-Copyright (C) 2003-2015 CompatibL
+Copyright (C) 2003-present CompatibL
 
 This file is part of .C++, a native C++ implementation of
-selected .NET class library APIs with built-in support for
-algorithmic differentiation (AD), available from
+popular .NET class library APIs developed to faciliate
+code reuse between C# and C++.
 
     http://github.com/dotcpp/dotcpp (source)
     http://dotcpp.org (documentation)
@@ -12,7 +12,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+   http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
