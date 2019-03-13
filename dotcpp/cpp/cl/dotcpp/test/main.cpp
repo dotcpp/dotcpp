b420f0916d379bb61714f7ef0abd3095c9feb05f
CompatibL <team@compatibl.com>
2019-03-12 20:31:12 -0400

    Removed Boost::Test related code from main.cpp.

--- dotcpp/cpp/cl/dotcpp/test/main.cpp
+++ dotcpp/cpp/cl/dotcpp/test/main.cpp
@@ -6,54 +6,5 @@ or distributed only in compliance with the terms of a written commercial
 license from CompatibL and with the inclusion of this copyright notice.
 */
 
-#include <cl/dotcpp/test/implement.hpp>
-#include <boost/test/unit_test.hpp>
-#include <boost/test/impl/unit_test_main.ipp>
-#include <cl/dotcpp/test/system/DoubleTest.hpp>
-#include <cl/dotcpp/test/system/ObjectTest.hpp>
-#include <cl/dotcpp/test/system/PtrTest.hpp>
-#include <cl/dotcpp/test/system/StringTest.hpp>
-#include <cl/dotcpp/test/system/Array1DTest.hpp>
-#include <cl/dotcpp/test/system/collections/generic/ListTest.hpp>
-#include <cl/dotcpp/test/system/PropertyTest.hpp>
-#include <cl/dotcpp/test/system/ReadOnlyPropertyTest.hpp>
-#include <cl/dotcpp/test/system/StaticPropertyTest.hpp>
-#include <cl/dotcpp/test/system/ReadOnlyStaticPropertyTest.hpp>
-#include <cl/dotcpp/test/system/ReflectionTest.hpp>
-
-// using namespace boost::unit_test_framework;
-
-#if defined _MSC_VER
-struct pause
-{
-    ~pause()
-    {
-        system("PAUSE");
-    }
-};
-
-static pause pause_;
-#endif
-
-test_suite* init_unit_test_suite(int, char*[])
-{
-    std::string header = "Cl.DotCpp.Test";
-    std::string rule = std::string(35, '=');
-
-    BOOST_TEST_MESSAGE(rule);
-    BOOST_TEST_MESSAGE(header);
-    BOOST_TEST_MESSAGE(rule);
-    test_suite* allTests = BOOST_TEST_SUITE(header);
-
-    allTests->add(cl::Array1DTest::GetTestSuite());
-    allTests->add(cl::ListTest::GetTestSuite());
-    allTests->add(cl::DoubleTest::DoubleTestSuite());
-    allTests->add(cl::PropertyTest::GetTestSuite());
-    allTests->add(cl::ReadOnlyPropertyTest::GetTestSuite());
-    allTests->add(cl::StaticPropertyTest::GetTestSuite());
-    allTests->add(cl::ReadOnlyStaticPropertyTest::GetTestSuite());
-    allTests->add(cl::PtrTest::GetTestSuite());
-    allTests->add(cl::ReflectionTest::GetTestSuite());
-
-    return allTests;
-}
+#define APPROVALS_CATCH
+#include <approvals/ApprovalTests.hpp>
