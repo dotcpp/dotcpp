37ed52a35692b1465c3b7d28ab3be2afe6e3753e
CompatibL <team@compatibl.com>
2019-04-03 19:51:53 -0400

    Added ExceptionTest.cpp.

---
+++ dotcpp/cpp/cl/dotcpp/test/system/ExceptionTest.cpp
@@ -0,0 +1,47 @@
+﻿/*
+Copyright (C) 2003-present CompatibL
+
+This file is part of .C++, a native C++ implementation of
+popular .NET class library APIs developed to facilitate
+code reuse between C# and C++.
+
+    http://github.com/dotcpp/dotcpp (source)
+    http://dotcpp.org (documentation)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <cl/dotcpp/test/implement.hpp>
+#include <approvals/ApprovalTests.hpp>
+#include <approvals/Catch.hpp>
+#include <cl/dotcpp/main/system/text/StringBuilder.hpp>
+#include <cl/dotcpp/main/system/Exception.hpp>
+
+namespace cl
+{
+    TEST_CASE("Smoke")
+    {
+        StringBuilder received = new_StringBuilder();
+
+        try
+        {
+            throw new_Exception("Runtime error");
+        }
+        catch(Exception e)
+        {
+            received->AppendLine(e.Message());
+        }
+
+        Approvals::verify(*received);
+    }
+}
