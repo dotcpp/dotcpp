4de4d9a055ade8db1719012f14d767d27a27b4d7
CompatibL <team@compatibl.com>
2019-08-06 19:41:13 -0400

    Added collection_base, enumerable_base.
    (cherry picked from commit 5297826bfaaec44e9c2ffa47dafc13d78bb3f8e1)
    (cherry picked from commit ecdfc3c4cee3948d7170eeb61a520f78055cdb6f)

---
+++ cpp/src/dotcpp/dot/system/collections/collection_base.cpp
@@ -0,0 +1,43 @@
+/*
+Copyright (C) 2015-present The DotCpp Authors.
+
+This file is part of .C++, a native C++ implementation of
+popular .NET class library APIs developed to facilitate
+code reuse between C# and C++.
+
+    http://github.com/dotcpp/dotcpp (source)
+    http://dotcpp.org (documentation)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#include <dot/precompiled.hpp>
+#include <dot/implement.hpp>
+#include <dot/system/collections/collection_base.hpp>
+#include <dot/system/type.hpp>
+
+namespace dot
+{
+    type_t collection_base_impl::typeof()
+    {
+        static type_t type_ = make_type_builder<collection_base_impl>("System.Collections", "ICollection")
+            ->build();
+        return type_;
+    }
+
+    type_t collection_base_impl::type()
+    {
+        return typeof();
+    }
+
+}
---
+++ cpp/src/dotcpp/dot/system/collections/enumerable_base.hpp
@@ -0,0 +1,53 @@
+/*
+Copyright (C) 2015-present The DotCpp Authors.
+
+This file is part of .C++, a native C++ implementation of
+popular .NET class library APIs developed to facilitate
+code reuse between C# and C++.
+
+    http://github.com/dotcpp/dotcpp (source)
+    http://dotcpp.org (documentation)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#pragma once
+
+#include <dot/declare.hpp>
+#include <dot/system/ptr.hpp>
+#include <dot/system/object_impl.hpp>
+#include <dot/system/type.hpp>
+
+namespace dot
+{
+
+    class enumerable_base_impl; using enumerable_base = ptr<enumerable_base_impl>;
+
+    class DOT_CLASS enumerable_base_impl : virtual public object_impl
+    {
+    public:
+
+        /// <summary>Get object from collection by index.</summary>
+        virtual object item(int index) = 0;
+
+        /// <summary>Get length of collection.</summary>
+        virtual int get_length() = 0;
+
+        /// <summary>Gets the type_t of the current instance.</summary>
+        virtual type_t type();
+
+        /// <summary>Gets the type_t of the enumerable_base_impl.</summary>
+        static type_t typeof();
+    };
+
+}
