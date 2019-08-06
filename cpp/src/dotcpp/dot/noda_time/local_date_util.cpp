8022b6031f0c1c8d31f048b1bc3fbae3f3f78e04
CompatibL <team@compatibl.com>
2019-08-05 21:42:38 -0400

    Updated copyright header for the new files.

--- cpp/src/dotcpp/dot/noda_time/local_date_util.cpp
+++ cpp/src/dotcpp/dot/noda_time/local_date_util.cpp
@@ -1,5 +1,12 @@
 /*
-Copyright (C) 2013-present The DataCentric Authors.
+Copyright (C) 2015-present The DotCpp Authors.
+
+This file is part of .C++, a native C++ implementation of
+popular .NET class library APIs developed to facilitate
+code reuse between C# and C++.
+
+    http://github.com/dotcpp/dotcpp (source)
+    http://dotcpp.org (documentation)
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
